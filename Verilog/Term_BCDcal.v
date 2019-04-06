6b8cedbbb0c42f33ba95f279a33468fe200c5451
18신대성 <esc990720@korea.ac.kr>
2019-04-06 13:44:48 +0900

    add invalid_BCD/overflow/underflow error detection

--- Verilog/Term_BCDcal.v
+++ Verilog/Term_BCDcal.v
@@ -1,21 +1,23 @@
-module Term_BCDcal(SW, HEX0, HEX1, HEX4, HEX5, HEX6, HEX7, LEDG);
+module Term_BCDcal(SW, HEX0, HEX1, HEX4, HEX5, HEX6, HEX7);
 
 	input [16:0] SW; // 16: mode select, 15:8: left num, 7:0: right num, 
 	output reg [0:6] HEX0, HEX1; // output segment
 	output reg [0:6] HEX4, HEX5; // right segment
 	output reg [0:6] HEX6, HEX7; // left segment
-	output [7:0] LEDG;
 	parameter Seg9 = 7'b000_1100; parameter Seg8 = 7'b000_0000; parameter Seg7 = 7'b000_1111; parameter Seg6 = 7'b010_0000; parameter Seg5 = 7'b010_0100;
 	parameter Seg4 = 7'b100_1100; parameter Seg3 = 7'b000_0110; parameter Seg2 = 7'b001_0010; parameter Seg1 = 7'b100_1111; parameter Seg0 = 7'b000_0001;
+	parameter SegErr = 7'b111_1111;
 	
 	reg [7:0] left, right;
 	wire [7:0] out;
 	reg [7:0] conv_right;
+	wire cal_err; // error when calculating
+	reg num_err; // error of BCD input
 	
 	//Convert conv (SW[7:0], conv_right);
-	Calculator cal (out, left, right, LEDG[6]);
-	assign LEDG[7] = 1'b1;
+	Calculator cal (out, left, right, cal_err);
 	
+	// setting values
 	always@(*)
 	begin
 		left = SW[15:8];
@@ -25,53 +27,69 @@ module Term_BCDcal(SW, HEX0, HEX1, HEX4, HEX5, HEX6, HEX7, LEDG);
 		endcase
 	end
 	
+	// to detect error of BCD input
 	always@(*)
 	begin
-		case(out[3:0]) // out segment
-			9:HEX0=Seg9;	8:HEX0=Seg8;	7:HEX0=Seg7;	6:HEX0=Seg6;
-			5:HEX0=Seg5;	4:HEX0=Seg4;	3:HEX0=Seg3;	2:HEX0=Seg2;			
-			1:HEX0=Seg1;	0:HEX0=Seg0;	default: HEX0 = 7'b1111111;
-		endcase
-		case(out[7:4])
-			9:HEX1=Seg9;	8:HEX1=Seg8;	7:HEX1=Seg7;	6:HEX1=Seg6;
-			5:HEX1=Seg5;	4:HEX1=Seg4;	3:HEX1=Seg3;	2:HEX1=Seg2;			
-			1:HEX1=Seg1;	0:HEX1=Seg0;	default: HEX1 = 7'b1111111;
+		if (SW[3:0] > 9 || SW[7:4] > 9 || SW[11:8] > 9 || SW[15:12] > 9)
+			num_err = 1'b1;
+		else num_err = 1'b0;
+	end
+	
+	// show variables to segment
+	always@(*)
+	begin
+		case({num_err, cal_err}) // invalid BCD/overflow/underflow detection
+			2'b00: begin
+				case(out[3:0]) // out segment
+					9:HEX0=Seg9;	8:HEX0=Seg8;	7:HEX0=Seg7;	6:HEX0=Seg6;
+					5:HEX0=Seg5;	4:HEX0=Seg4;	3:HEX0=Seg3;	2:HEX0=Seg2;			
+					1:HEX0=Seg1;	0:HEX0=Seg0;	default: HEX0 = SegErr;
+				endcase
+				case(out[7:4])
+					9:HEX1=Seg9;	8:HEX1=Seg8;	7:HEX1=Seg7;	6:HEX1=Seg6;
+					5:HEX1=Seg5;	4:HEX1=Seg4;	3:HEX1=Seg3;	2:HEX1=Seg2;			
+					1:HEX1=Seg1;	0:HEX1=Seg0;	default: HEX1 = SegErr;
+				endcase
+			end
+			default: begin
+				HEX0 = SegErr;
+				HEX1 = SegErr;
+			end
 		endcase
 		case(SW[3:0]) // right segment
 			9:HEX4=Seg9;    8:HEX4=Seg8;	7:HEX4=Seg7;	6:HEX4=Seg6;
 			5:HEX4=Seg5;	4:HEX4=Seg4;	3:HEX4=Seg3;	2:HEX4=Seg2;
-			1:HEX4=Seg1;	0:HEX4=Seg0;  	default: HEX4 = 7'b1111111;
+			1:HEX4=Seg1;	0:HEX4=Seg0;  	default: HEX4 = SegErr;
 		endcase
 		case(SW[7:4])
 			9:HEX5=Seg9;	8:HEX5=Seg8;	7:HEX5=Seg7;	6:HEX5=Seg6;
 			5:HEX5=Seg5;	4:HEX5=Seg4;	3:HEX5=Seg3;	2:HEX5=Seg2;
-			1:HEX5=Seg1;	0:HEX5=Seg0;	default: HEX5 = 7'b1111111;
+			1:HEX5=Seg1;	0:HEX5=Seg0;	default: HEX5 = SegErr;
 		endcase
 		case(SW[11:8]) // left segment
 			9:HEX6=Seg9;	8:HEX6=Seg8;	7:HEX6=Seg7;	6:HEX6=Seg6;
 			5:HEX6=Seg5;	4:HEX6=Seg4;	3:HEX6=Seg3;	2:HEX6=Seg2;			
-			1:HEX6=Seg1;	0:HEX6=Seg0;	default: HEX6 = 7'b1111111;
+			1:HEX6=Seg1;	0:HEX6=Seg0;	default: HEX6 = SegErr;
 		endcase
 		case(SW[15:12])
 			9:HEX7=Seg9;	8:HEX7=Seg8;	7:HEX7=Seg7;	6:HEX7=Seg6;
 			5:HEX7=Seg5;	4:HEX7=Seg4;	3:HEX7=Seg3;	2:HEX7=Seg2;			
-			1:HEX7=Seg1;	0:HEX7=Seg0;	default: HEX7 = 7'b1111111;
+			1:HEX7=Seg1;	0:HEX7=Seg0;	default: HEX7 = SegErr;
 		endcase
 	end
 	
 endmodule
 
 // calculate whatever it is
-module Calculator(outBCD, leftBCD, rightBCD, LED);
+module Calculator(outBCD, leftBCD, rightBCD, c_err);
 
 	input [7:0] leftBCD, rightBCD;
 	output [7:0] outBCD;
-	output LED;
-	wire c_out0, c_out1;
+	output wire c_err; // if this is 1'b1, return overflow/underflow error
+	wire c_out0;
 	
 	Full_Adder fulladd1(outBCD[3:0], c_out0, leftBCD[3:0], rightBCD[3:0], 1'b0);
-	assign LED = c_out0;
-	Full_Adder fulladd2(outBCD[7:4], c_out1, leftBCD[7:4], rightBCD[7:4], c_out0);
+	Full_Adder fulladd2(outBCD[7:4], c_err, leftBCD[7:4], rightBCD[7:4], c_out0);
 	
 endmodule
 
